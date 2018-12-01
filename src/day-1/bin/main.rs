cee1029abcad64f313e1fe8391d138a01f719761
Nick Spain <nicholas.spain96@gmail.com>
2018-12-01 16:29:02 +1100

    Initial commit

---
+++ src/day-1/bin/main.rs
@@ -0,0 +1,10 @@
+/** Day 1: Chronal Calibration
+ *
+ * Determine the resulting frequency after all changes in frequency
+ * have been applied, starting from a frequency of zero.
+ */
+
+
+fn main() {
+    println!("Hello, world!");
+}
