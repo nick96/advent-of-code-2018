a9d1d01710d24147167e456201b162461cf6fdd8
Nick Spain <nicholas.spain96@gmail.com>
2018-12-01 21:10:53 +1100

    Move tests to submodule and add the challenge questions as a test

--- src/day-1/bin/main.rs
+++ src/day-1/bin/main.rs
@@ -69,92 +69,114 @@ fn find_repeated_freq<R: Read + Seek>(mut reader: R) -> i32 {
     }
 }
 
-#[test]
-fn part1_example1() {
-    use std::io::Cursor;
-    let deltas = vec!["+1", "-2", "+3", "+1"];
-    let input = deltas.join("\n");
-    let buf = Cursor::new(input.as_bytes());
-    let freq = determine_freq(buf);
-    assert_eq!(freq, 3);
-}
+#[cfg(test)]
+mod day1tests {
+    use super::*;
 
-#[test]
-fn part1_example2() {
-    use std::io::Cursor;
-    let deltas = vec!["+1", "+1", "+1"];
-    let input = deltas.join("\n");
-    let buf = Cursor::new(input.as_bytes());
-    let freq = determine_freq(buf);
-    assert_eq!(freq, 3);
-}
+    #[test]
+    fn part1_example1() {
+        use std::io::Cursor;
+        let deltas = vec!["+1", "-2", "+3", "+1"];
+        let input = deltas.join("\n");
+        let buf = Cursor::new(input.as_bytes());
+        let freq = determine_freq(buf);
+        assert_eq!(freq, 3);
+    }
 
-#[test]
-fn part1_example3() {
-    use std::io::Cursor;
-    let deltas = vec!["+1", "+1", "-2"];
-    let input = deltas.join("\n");
-    let buf = Cursor::new(input.as_bytes());
-    let freq = determine_freq(buf);
-    assert_eq!(freq, 0);
-}
+    #[test]
+    fn part1_example2() {
+        use std::io::Cursor;
+        let deltas = vec!["+1", "+1", "+1"];
+        let input = deltas.join("\n");
+        let buf = Cursor::new(input.as_bytes());
+        let freq = determine_freq(buf);
+        assert_eq!(freq, 3);
+    }
 
-#[test]
-fn part1_example4() {
-    use std::io::Cursor;
-    let deltas = vec!["-1", "-2", "-3"];
-    let input = deltas.join("\n");
-    let buf = Cursor::new(input.as_bytes());
-    let freq = determine_freq(buf);
-    assert_eq!(freq, -6);
-}
+    #[test]
+    fn part1_example3() {
+        use std::io::Cursor;
+        let deltas = vec!["+1", "+1", "-2"];
+        let input = deltas.join("\n");
+        let buf = Cursor::new(input.as_bytes());
+        let freq = determine_freq(buf);
+        assert_eq!(freq, 0);
+    }
 
-#[test]
-fn part2_example1() {
-    use std::io::Cursor;
-    let deltas = vec!["+1", "-2", "+3", "+1", "+1", "-2"];
-    let input = deltas.join("\n");
-    let buf = Cursor::new(input.as_bytes());
-    let freq = find_repeated_freq(buf);
-    assert_eq!(freq, 2);
-}
+    #[test]
+    fn part1_example4() {
+        use std::io::Cursor;
+        let deltas = vec!["-1", "-2", "-3"];
+        let input = deltas.join("\n");
+        let buf = Cursor::new(input.as_bytes());
+        let freq = determine_freq(buf);
+        assert_eq!(freq, -6);
+    }
 
-#[test]
-fn part2_example2() {
-    use std::io::Cursor;
-    let deltas = vec!["+1", "-1"];
-    let input = deltas.join("\n");
-    let buf = Cursor::new(input.as_bytes());
-    let freq = find_repeated_freq(buf);
-    assert_eq!(freq, 0);
-}
+    #[test]
+    fn part1_challenge() {
+        let filename = "input.txt";
+        let fh = File::open(filename).expect("file not found");
+        let freq = determine_freq(fh);
+        assert_eq!(freq, 510);
+    }
 
-#[test]
-fn part2_example3() {
-    use std::io::Cursor;
-    let deltas = vec!["+3", "+3", "+4", "-2", "-4"];
-    let input = deltas.join("\n");
-    let buf = Cursor::new(input.as_bytes());
-    let freq = find_repeated_freq(buf);
-    assert_eq!(freq, 10);
-}
+    #[test]
+    fn part2_example1() {
+        use std::io::Cursor;
+        let deltas = vec!["+1", "-2", "+3", "+1", "+1", "-2"];
+        let input = deltas.join("\n");
+        let buf = Cursor::new(input.as_bytes());
+        let freq = find_repeated_freq(buf);
+        assert_eq!(freq, 2);
+    }
 
-#[test]
-fn part2_example4() {
-    use std::io::Cursor;
-    let deltas = vec!["-6", "+3", "+8", "+5", "-6"];
-    let input = deltas.join("\n");
-    let buf = Cursor::new(input.as_bytes());
-    let freq = find_repeated_freq(buf);
-    assert_eq!(freq, 5);
-}
+    #[test]
+    fn part2_example2() {
+        use std::io::Cursor;
+        let deltas = vec!["+1", "-1"];
+        let input = deltas.join("\n");
+        let buf = Cursor::new(input.as_bytes());
+        let freq = find_repeated_freq(buf);
+        assert_eq!(freq, 0);
+    }
 
-#[test]
-fn part2_example5() {
-    use std::io::Cursor;
-    let deltas = vec!["+7", "+7", "-2", "-7", "-4"];
-    let input = deltas.join("\n");
-    let buf = Cursor::new(input.as_bytes());
-    let freq = find_repeated_freq(buf);
-    assert_eq!(freq, 14);
+    #[test]
+    fn part2_example3() {
+        use std::io::Cursor;
+        let deltas = vec!["+3", "+3", "+4", "-2", "-4"];
+        let input = deltas.join("\n");
+        let buf = Cursor::new(input.as_bytes());
+        let freq = find_repeated_freq(buf);
+        assert_eq!(freq, 10);
+    }
+
+    #[test]
+    fn part2_example4() {
+        use std::io::Cursor;
+        let deltas = vec!["-6", "+3", "+8", "+5", "-6"];
+        let input = deltas.join("\n");
+        let buf = Cursor::new(input.as_bytes());
+        let freq = find_repeated_freq(buf);
+        assert_eq!(freq, 5);
+    }
+
+    #[test]
+    fn part2_example5() {
+        use std::io::Cursor;
+        let deltas = vec!["+7", "+7", "-2", "-7", "-4"];
+        let input = deltas.join("\n");
+        let buf = Cursor::new(input.as_bytes());
+        let freq = find_repeated_freq(buf);
+        assert_eq!(freq, 14);
+    }
+
+    #[test]
+    fn part2_challenge() {
+        let filename = "input.txt";
+        let fh = File::open(filename).expect("file not found");
+        let freq = find_repeated_freq(fh);
+        assert_eq!(freq, 69074);
+    }
+
 }
